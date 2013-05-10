75044c9f1f991e428a46165eee8388de05cfd4f0
Ko Hsiang Yu <kohsiangyu@gmail.com>
2013-05-11 02:02:44 +0800

    Move make db connect as a standalone module.

---
+++ install/connectdb.php
@@ -0,0 +1,9 @@
+<?php
+	$con=mysqli_connect("localhost","libmanager","pFJpjtbc7FWFaAYn");
+
+	// Check connection
+	if (mysqli_connect_errno()){
+		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
+
+?>
