a041443ee66ca87f4bae72eb4482592f2802ee67
Ko Hsiang Yu <kohsiangyu@gmail.com>
2013-05-12 01:46:09 +0800

    Add bootstrap modules and normalize.css module and two login interfaces

---
+++ view/login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<?php
+?>
+<html>
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+
+		<!-- bootstrap module -->
+		<link rel="stylesheet" type="text/css" href="../thirdparty/bootstrap/css/bootstrap.css" />
+		<script type="text/javascript" src="../thirdparty/bootstrap/js/bootstrap.js"></script>
+
+		<!-- Todo -->
+		<title>Welcome to my library</title>
+		
+	</head>
+	<body>
+		<form name="input" action="login.php" method="post">
+			<label for="user">Name</label>
+			<input type="text" name="user" id="user"><br/>
+
+			<label for="pass">Password</label>
+			<input type="password" name="pass" id="pass"><br/>
+
+			<input type="submit" value="Hack it">
+		</form>
+	</body>
+</html>
