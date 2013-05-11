89a6e7bcbfffd1477dc1fad378c291ab76a45201
Ko Hsiang Yu <kohsiangyu@gmail.com>
2013-05-11 22:36:12 +0800

    Add Test Files on JSON

---
+++ tests/json_test3/json-data.php
@@ -0,0 +1,4 @@
+<?php
+$arr = array ('item1'=>"I love jquery4u",'item2'=>"You love jQuery4u",'item3'=>"We love jQuery4u");
+echo json_encode($arr);
+?>
---
+++ tests/json_test3/json-jquery.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	//attach a jQuery live event to the button
+	$('#getdata-button').live('click', function(){
+		$.ajax({
+			type: "GET",
+			url: 'json-data.php',
+			async: false,
+			beforeSend: function(x){
+					if(x && x.overrideMimeType){
+						x.overrideMimeType("application/j-son;charset=UTF-8");
+					}
+				},
+			dataType: "json",
+			success: function(data){
+				//alert(data); //uncomment this for debug
+				//alert (data.item1+" "+data.item2+" "+data.item3); //further debug
+				$('#showdata').html("<p>item1="+data.item1+" item2="+data.item2+" item3="+data.item3+"</p>");
+			}
+		});
+	});
+});
